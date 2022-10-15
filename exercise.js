97cad6b6e7bf5ebadfc7a7cdb2bd4274bd099614
Asdren JERLIU <114486010+asdreen@users.noreply.github.com>
2022-10-15 18:38:53 +0200

    11

---
+++ exercise.js
@@ -0,0 +1,22 @@
+/*    exercise 21    */
+const x = "John";
+const y = "Doe";
+console.log(x + " " + "W" + " " + y);
+
+/*    exercise 22    */
+const cv = {
+    name: "Asdren",
+    surname: "Jerliu",
+    email: "asdren.jerliu@gmail.com",
+};
+console.log(cv);
+
+/*    exercise 23    */
+delete cv.email;
+console.log(cv);
+
+/*    exercise 24    */
+const arrayOfStrings = ["3", "BMW", "-5", "Linux", "0", "75", "Alfa Romeo", "121", "Volvo", "4"];
+console.log(arrayOfStrings);
+
+/*    exercise 25    */
